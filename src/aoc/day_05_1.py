fb32b6fb9238691fc60779331e2f94956bcf6297
Ogaday Willers Moore <w.ogaday@gmail.com>
2023-12-05 09:39:05 +0000

    Feat: Add rough version of day 5 part 1
    Could do with tidying up variable names (window vs. length etc) and
    handling data parsing more tidily.

--- src/aoc/day_05_1.py
+++ src/aoc/day_05_1.py
@@ -1,16 +1,60 @@
-def make_mapper(chunk):
-    mapper = {}
-    for row in chunk.split("\n")[1:]:
-        dest_start, source_start, window = [int(val) for val in row.split()]
-        for i in range(window):
-            mapper[source_start + i] = dest_start + i
-    return mapper
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class RangeSpec:
+    source_start: int
+    dest_start: int
+    length: int
+
+    @property
+    def source_end(self):
+        return self.source_start + self.length
+
+    def dest_end(self):
+        return self.dest_start + self.length
+
+    def __contains__(self, item: Any) -> bool:
+        return self.source_start <= item < self.source_end
+
+    def map(self, item: int) -> int:
+        if item in self:
+            offset = item - self.source_start
+            return self.dest_start + offset
+        raise KeyError(item)
+
+
+class Mapper:
+    def __init__(self, ranges: list[RangeSpec]):
+        self.ranges = ranges
+
+    def map(self, item: int) -> int:
+        for spec in self.ranges:
+            if item in spec:
+                mapped = spec.map(item)
+                break
+        else:
+            mapped = item
+        return mapped
 
 
 if __name__ == "__main__":
     with open("inputs/day_05.txt") as f:
         chunks = f.read().split("\n\n")
     seeds = [int(val) for val in chunks.pop(0).split()[1:]]
+    mappers = []
     for chunk in chunks:
-        mapper = make_mapper(chunk)
-        seeds = [mapper.get(seed, seed) for seed in seeds]
+        specs = []
+        for row in chunk.strip().split("\n")[1:]:
+            dest_start, source_start, window = [int(val) for val in row.split()]
+            specs.append(
+                RangeSpec(
+                    source_start=source_start, dest_start=dest_start, length=window
+                )
+            )
+        mappers.append(Mapper(ranges=specs))
+    new_seeds = seeds
+    for mapper in mappers:
+        new_seeds = [mapper.map(seed) for seed in new_seeds]
+    print(min(new_seeds))
